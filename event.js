2ac266d050ee1b120cd11c98abc39dc0722438cc
Osayu-Sayu-Sayu <251zs01790@student.zen.ac.jp>
2026-01-14 20:41:46 +0900

    Create event.js

---
+++ event.js
@@ -0,0 +1,13 @@
+
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidをもつHTML要素を取得し、定数に代入する
+const text = document.getElementById('newText');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  //newTextに「ボタンをクリックしました」というテキストを追加する
+  text.textContent = 'ボタンをクリックしました';
+  console.log(text);
+})
